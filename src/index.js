664654aadf7df34a42d5dce8468c1dbe422f952a
Chirath R <chirath.02@gmail.com>
2019-04-10 11:12:00 +0530

    Add option to go back a step

--- src/index.js
+++ src/index.js
@@ -93,9 +93,25 @@ class Game extends React.Component {
         });
     }
 
+    jumpTo(move) {
+        let history = this.state.history.splice(0, move + 1);
+        let nextPlayer = (move % 2)? 'O': 'X';
+        this.setState({history: history, nextPlayer: nextPlayer});
+    }
+
     render() {
         const squares =  this.state.history[this.state.history.length - 1];
         const winner = calculateWinner(squares);
+
+        const moves = this.state.history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -112,7 +128,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
